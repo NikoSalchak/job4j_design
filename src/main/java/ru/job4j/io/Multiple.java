427c349f700eaa63ba810f16a82d9228b8cab208
NikoSalchak <nikosalchak@gmail.com>
2024-02-14 14:04:08 +0700

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/Multiple.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class Multiple {
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("data/multipleResult.txt")) {
+            Integer a = 1 * 2;
+            output.write("1 * 2 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 3;
+            output.write("1 * 3 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 4;
+            output.write("1 * 4 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 5;
+            output.write("1 * 5 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 6;
+            output.write("1 * 6 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 7;
+            output.write("1 * 7 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 8;
+            output.write("1 * 8 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+            a = 1 * 9;
+            output.write("1 * 9 = ".getBytes());
+            output.write(a.toString().getBytes());
+            output.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
