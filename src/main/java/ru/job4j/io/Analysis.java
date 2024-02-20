4e9a1269817b084ea70c89532fdafd3201d90f79
NikoSalchak <nikosalchak@gmail.com>
2024-02-20 13:10:26 +0700

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
+             PrintWriter printWriter = new PrintWriter(new FileOutputStream(target))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            boolean isWork = true;
+            while ((line = bufferedReader.readLine()) != null) {
+                if ((line.contains("400") || line.contains("500")) && isWork) {
+                    line = line.split(" ")[1];
+                    sb.append(line + ";");
+                    isWork = false;
+                }
+                if ((line.contains("200") || line.contains("300")) && !isWork) {
+                    line = line.split(" ")[1];
+                    sb.append(line + ";");
+                    isWork = true;
+                    sb.append("\n");
+                }
+            }
+            System.out.println(sb);
+            printWriter.print(sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
