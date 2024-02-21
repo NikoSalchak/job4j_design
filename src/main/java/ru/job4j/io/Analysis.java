8ec71fae6157c3ed626bc53a4c3dac96b0b47155
NikoSalchak <nikosalchak@gmail.com>
2024-02-21 16:57:20 +0700

    7. Анализ доступности сервера [#859 #482295]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,23 +6,18 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
              PrintWriter printWriter = new PrintWriter(new FileOutputStream(target))) {
-            StringBuilder sb = new StringBuilder();
             String line;
-            boolean isWork = true;
+            boolean isWork = false;
             while ((line = bufferedReader.readLine()) != null) {
-                if ((line.contains("400") || line.contains("500")) && isWork) {
+                if (((line.contains("400") || line.contains("500")) && !isWork)
+                        || ((line.contains("200") || line.contains("300")) && isWork)) {
+                    isWork = !isWork;
                     line = line.split(" ")[1];
-                    sb.append(line + ";");
-                    isWork = false;
-                }
-                if ((line.contains("200") || line.contains("300")) && !isWork) {
-                    line = line.split(" ")[1];
-                    sb.append(line + ";");
-                    isWork = true;
-                    sb.append("\n");
+                    printWriter.printf("%s%s", line, ";");
+                    line = isWork ? "" : System.lineSeparator();
+                    printWriter.print(line);
                 }
             }
-            printWriter.print(sb);
         } catch (IOException e) {
             e.printStackTrace();
         }
