16b7ec3ffed3f178f37ea2c52ca686531008c1c6
NikoSalchak <nikosalchak@gmail.com>
2024-02-24 22:49:07 +0700

    12. Валидация параметров запуска [#246865 #483016]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,28 +9,31 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+    private static Path start;
+    private static BasicFileAttributes attrs;
+
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
-    private static void validateArgs(String[] args) {
+    private static void validateArgs(String[] args) throws IOException {
         if (args.length == 0) {
             throw new IllegalArgumentException("program arguments were not found, please add program arguments");
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        validateArgs(args);
-        Path start = Paths.get(args[0]);
-        BasicFileAttributes attrs = Files.readAttributes(start, BasicFileAttributes.class);
+        Search.start = Paths.get(args[0]);
+        Search.attrs = Files.readAttributes(start, BasicFileAttributes.class);
         if (!Files.exists(start)) {
             throw new IllegalArgumentException(String.format("Not Exist %s", start.toAbsolutePath()));
         }
         if (attrs.isRegularFile()) {
             throw new IllegalArgumentException(String.format("Not directory %s", start.toAbsolutePath()));
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        validateArgs(args);
         search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 }
