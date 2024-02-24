45471f14a6c21b2cb09f79d26562f3c218de57ab
NikoSalchak <nikosalchak@gmail.com>
2024-02-24 18:06:37 +0700

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,38 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<Path>> map = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(Files.size(file), file.getFileName().toString());
+        if (map.containsKey(fileProperty)) {
+            map.get(fileProperty).add(file.toAbsolutePath());
+        } else {
+            map.put(fileProperty, new ArrayList<>());
+            map.get(fileProperty).add(file.toAbsolutePath());
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    public void resultMap() {
+        map = map.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        for (Map.Entry<FileProperty, List<Path>> fileProperty : map.entrySet()) {
+            String newLine = System.lineSeparator();
+            System.out.printf("%s - %s%s%s", fileProperty.getKey().getName(),
+                    fileProperty.getKey().getSize(), "byte", newLine);
+            for (Path path : fileProperty.getValue()) {
+                System.out.printf("%s%s", path, newLine);
+            }
+        }
+    }
+}
