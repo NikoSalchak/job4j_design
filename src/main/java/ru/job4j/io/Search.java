c82dfc5bb1d1e0c8d0a4269f471f92de6b9c742a
NikoSalchak <nikosalchak@gmail.com>
2024-02-24 21:15:58 +0700

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,9 +1,12 @@
 package ru.job4j.io;
 
+import ru.job4j.nio.BasicFileAttributeView;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -14,8 +17,22 @@ public class Search {
         return searcher.getPaths();
     }
 
+    private static void validateArgs(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("program arguments were not found, please add program arguments");
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        BasicFileAttributes attrs = Files.readAttributes(start, BasicFileAttributes.class);
+        if (!Files.exists(start)) {
+            throw new IllegalArgumentException(String.format("Not Exist %s", start.toAbsolutePath()));
+        }
+        if (attrs.isRegularFile()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toAbsolutePath()));
+        }
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 }
