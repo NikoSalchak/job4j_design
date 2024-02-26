95e60b6938fb410e2eaa79116eb18d502cbab150
NikoSalchak <nikosalchak@gmail.com>
2024-02-26 15:15:44 +0700

    13. Именованные аргументы [#295518 #483166]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -17,27 +17,31 @@ public class ArgsName {
     private void parse(String[] args) {
         for (String s : args) {
             String[] substrings = s.split("=", 2);
-            if (!s.startsWith("-")) {
-                throw new IllegalArgumentException(
-                        String.format("Error: This argument '%s' does not start with a '-' character", s)
-                );
-            }
-            if (!s.contains("=")) {
-                throw new IllegalArgumentException(
-                        String.format("Error: This argument '%s' does not contain an equal sign", s)
-                );
-            }
-            if ("".equals(substrings[0].substring(1))) {
-                throw new IllegalArgumentException(
-                        String.format("Error: This argument '%s' does not contain a key", s)
-                );
-            }
-            if ("".equals(substrings[1])) {
-                throw new IllegalArgumentException(
-                        String.format("Error: This argument '%s' does not contain a value", s)
-                );
-            }
+            validateSubstrings(s, substrings);
             values.put(substrings[0].substring(1), substrings[1]);
+        }
+    }
+
+    private void validateSubstrings(String s, String[] substrings) {
+        if (!s.startsWith("-")) {
+            throw new IllegalArgumentException(
+                    String.format("Error: This argument '%s' does not start with a '-' character", s)
+            );
+        }
+        if (!s.contains("=")) {
+            throw new IllegalArgumentException(
+                    String.format("Error: This argument '%s' does not contain an equal sign", s)
+            );
+        }
+        if ("".equals(substrings[0].substring(1))) {
+            throw new IllegalArgumentException(
+                    String.format("Error: This argument '%s' does not contain a key", s)
+            );
+        }
+        if ("".equals(substrings[1])) {
+            throw new IllegalArgumentException(
+                    String.format("Error: This argument '%s' does not contain a value", s)
+            );
         }
     }
 
