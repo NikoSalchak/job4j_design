35c7fcd9c06b13ae219b99362a9be4aff92047eb
NikoSalchak <nikosalchak@gmail.com>
2024-03-09 19:51:19 +0700

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Niko";
+        int age = 33;
+        float laptopSize = 15.6F;
+        double monitorSize = 24.5;
+        byte completeTasks = 127;
+        short requiredTasks = 32767;
+        long desiredSalary = 300000L;
+        boolean achieved = false;
+        LOG.debug("User info name : {}, age : {}, laptop size : {}", name, age, laptopSize);
+        LOG.debug("monitor size : {}, completed tasks : {}, required tasks : {}", monitorSize, completeTasks, requiredTasks);
+        LOG.debug("desired salary : {}, Did you achieve your goal ? {} ", desiredSalary, achieved);
     }
 }
