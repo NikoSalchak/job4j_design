34d19521ff8a08c803d5931fa540d680a4f8ec28
NikoSalchak <nikosalchak@gmail.com>
2024-03-10 17:25:33 +0700

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,17 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Niko";
-        int age = 33;
-        float laptopSize = 15.6F;
-        double monitorSize = 24.5;
-        byte completeTasks = 127;
-        short requiredTasks = 32767;
-        long desiredSalary = 300000L;
-        boolean achieved = false;
-        char avScore = 'c';
-        LOG.debug("User info name : {}, age : {}, laptop size : {}", name, age, laptopSize);
-        LOG.debug("monitor size : {}, completed tasks : {}, required tasks : {}", monitorSize, completeTasks, requiredTasks);
-        LOG.debug("desired salary : {}, Did you achieve your goal ? {}, average score : {}", desiredSalary, achieved, avScore);
+        try {
+            throw new Exception("Not supported code");
+        } catch ( Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
