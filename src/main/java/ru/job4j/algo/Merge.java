509a96fa07202628de491d885a068a95e9e16a35
NikoSalchak <nikosalchak@gmail.com>
2024-10-04 18:36:07 +0700

    Сортировка слиянием [#504997]

---
+++ src/main/java/ru/job4j/algo/Merge.java
@@ -0,0 +1,45 @@
+package ru.job4j.algo;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public static int[] mergeSort(int[] array) {
+        int[] result = array;
+        int n = array.length;
+        if (n > 1) {
+            int[] left = mergeSort(Arrays.copyOfRange(array, 0, n / 2));
+            int[] right = mergeSort(Arrays.copyOfRange(array, n / 2, n));
+            result = merge(left, right);
+        }
+        return result;
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int indexLeft = 0;
+        int indexRight = 0;
+        int i = 0;
+        while (i < rsl.length ) {
+            if (indexLeft == left.length) {
+              rsl[i] = right[indexRight++];
+            } else if (indexRight == right.length) {
+                rsl[i] = left[indexLeft++];
+            }
+            else if (left[indexLeft] <= right[indexRight]) {
+                rsl[i] = left[indexLeft];
+                indexLeft++;
+            } else if (left[indexLeft] > right[indexRight]) {
+                rsl[i] = right[indexRight];
+                indexRight++;
+            }
+            i++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
+        System.out.println(Arrays.toString(mergeSort(array)));
+    }
+}
---
+++ src/test/java/ru/job4j/algo/MergeTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.algo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MergeTest {
+
+    @Test
+    void whenSortedThenOk(){
+        int[] array = {-13, 2, 3, 4, 4, 6, 8, 10};
+        assertThat(Merge.mergeSort(array)).containsExactly(-13, 2, 3, 4, 4, 6, 8, 10);
+    }
+    @Test
+    void whenSortedThenOk2(){
+        int[] array = {1, 2, 3, 4, 5};
+        assertThat(Merge.mergeSort(array)).containsSequence(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    void whenSorThenSEqual() {
+        int[] array = {5, 5, 5, 5, 5};
+        assertThat(Merge.mergeSort(array)).isEqualTo(new int[]{5, 5, 5, 5, 5});
+    }
+
+    @Test
+    void whenSortThenFalse() {
+        int[] array = {10, 9, 8, 7, 6};
+        assertThat(Merge.mergeSort(array)).isNotEqualTo(new int[]{10, 9, 8, 7, 6});
+    }
+
+
+}
